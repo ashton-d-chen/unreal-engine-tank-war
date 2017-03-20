a97f4c28da18975dc3302e12ce3336115db2dedb
Ashton Chen <contact@ashtonchen.com>
2017-03-19 19:39:48 -0700

    TW007 - Add C++ parent class for tank pawn and player controller

---
+++ TankWar/Source/TankWar/TankPlayerController.cpp
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankWar.h"
+#include "TankPlayerController.h"
+
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	auto ControlledTank = GetControlledTank();
+
+	if (ControlledTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController possessing: %s"), *(ControlledTank->GetName()));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController not possessing a tank"));
+	}
+
+	UE_LOG(LogTemp, Warning, TEXT("PlayerConroller Begin Play"));
+}
+
+ATank* ATankPlayerController::GetControlledTank() const
+{
+	return Cast<ATank>(GetPawn());
+}
+
+
+
