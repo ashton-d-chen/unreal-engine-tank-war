306142079946d07937c0ea79130da1176240d7c4
Ashton Chen <ashton.d.chen@gmail.com>
2017-11-08 20:32:50 -0800

    Adding track component

---
+++ TankWar/Source/TankWar/Private/TankTrack.cpp
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankWar.h"
+#include "TankTrack.h"
+
+
+
+void UTankTrack::SetThrottle(float Throttle)
+{
+	auto Time = GetWorld()->GetTimeSeconds();
+	auto Name = GetName();
+	UE_LOG(LogTemp, Warning, TEXT("%f: throttle is %s"), *Name);
+}
+
